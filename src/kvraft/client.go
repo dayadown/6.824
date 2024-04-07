0fcb911ea3d87917c2956dbd72e7ef1335edb399
“dayadown” <1593441797@qq.com>
2024-04-08 00:09:33 +0800

    完成3A, 每个客户端均有一个单调递增的标识表示自己已经执行到第几个自己发出的操作，每个kvraft服务器在执行完操作后也会对该信息进行更新，表明自己知道的某客户端的到某一操作已经执行，防止冗余请求执行多次。 对于客户端的并发请求，会在raft的start函数处争夺互斥锁，抢占不同的日志下标，从而实现线性的日志。 遇到的问题如下： 1.等待raft共识的时间不能少于100ms,因为心跳的间隔才100ms,共识还没开始就宣布共识超时是不现实的，最好是心跳的两倍 2.只有leader在收到applych时才会去通知其相应等待的线程，而非leader就只需要执行收到的命令即可 3.用start函数返回的index作为等待通道的索引会因为网络分区出现不一致的问题，不同的分区的start得到的index可能是相同的，但是操作的是不同的，如果分区快速恢复那么少数派分区的操作会被覆盖并由于index相同而返回成功信息，导致不一致，这就需要在得到成功的通道信息时判断是否是当时发出的操作，再给客户端返回信息，具体体现在server的get函数的case <-sh处

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,18 @@
 package kvraft
 
-import "../labrpc"
-import "crypto/rand"
+import (
+	"../labrpc"
+	"crypto/rand"
+	mrand "math/rand"
+)
 import "math/big"
-
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	clientId  int64 //客户端唯一标识
+	commandId int   //客户端命令的id,单调增
+	leaderId  int   //所知道的leader的id
 }
 
 func nrand() int64 {
@@ -21,6 +26,9 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()                     //随机生成
+	ck.leaderId = mrand.Intn(len(ck.servers)) //初始时随机一个
+	ck.commandId = 0                          //从0开始递增
 	return ck
 }
 
@@ -37,9 +45,40 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // arguments. and reply must be passed as a pointer.
 //
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+	ck.commandId++
+	args := GetArgs{
+		Key:       key,
+		ClientId:  ck.clientId,
+		CommandId: ck.commandId,
+	}
+	leaderId := ck.leaderId
+	for {
+		reply := GetReply{}
+		ok := ck.servers[leaderId].Call("KVServer.Get", &args, &reply)
+		//println(ck.clientId, "发送操作 Get", key, "给", leaderId, "回复是", reply.Err, reply.Value)
+		if ok {
+			switch reply.Err {
+			case OK:
+				{
+					ck.leaderId = leaderId
+					return reply.Value
+				}
+			case ErrNoKey:
+				{
+					ck.leaderId = leaderId
+					return ""
+				}
+			case ErrWrongLeader:
+				{
+					leaderId = (leaderId + 1) % len(ck.servers)
+					continue
+				}
+			}
+		}
+		//节点没有回应
+		leaderId = (leaderId + 1) % len(ck.servers)
+	}
 }
 
 //
@@ -54,6 +93,35 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	ck.commandId++
+	args := PutAppendArgs{
+		Key:       key,
+		Value:     value,
+		Op:        op,
+		ClientId:  ck.clientId,
+		CommandId: ck.commandId,
+	}
+	leaderId := ck.leaderId
+	for {
+		reply := PutAppendReply{}
+		ok := ck.servers[leaderId].Call("KVServer.PutAppend", &args, &reply)
+		//println(ck.clientId, "发送操作", op, key, value, "给", leaderId, "回复是", reply.Err)
+		if ok {
+			switch reply.Err {
+			case OK:
+				{
+					ck.leaderId = leaderId
+					return
+				}
+			case ErrWrongLeader:
+				{
+					leaderId = (leaderId + 1) % len(ck.servers)
+					continue
+				}
+			}
+		}
+		leaderId = (leaderId + 1) % len(ck.servers)
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -3,10 +3,11 @@ package kvraft
 import (
 	"../labgob"
 	"../labrpc"
-	"log"
 	"../raft"
+	"log"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 const Debug = 0
@@ -18,11 +19,17 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+//raft日志中要存储的数据
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	CommandId int
+	Key       string
+	Value     string
+	ClientId  int64
+	OpType    string
 }
 
 type KVServer struct {
@@ -35,15 +42,184 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	clientIdCommandId map[int64]int     //记录特定客户端在这台server上的命令位置	clientId / seqId
+	indexOpChan       map[int]chan Op   //通过index匹配raft层传来的信息	index / chan(Op)
+	kvMap             map[string]string // 存储持久化的KV键值对	K / V
 }
 
+func (kv *KVServer) getWaitCh(index int) chan Op {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	ch, exist := kv.indexOpChan[index]
+	if !exist {
+		kv.indexOpChan[index] = make(chan Op, 1)
+		ch = kv.indexOpChan[index]
+	}
+	return ch
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	if kv.killed() {
+		reply.Err = ErrWrongLeader
+		return
+	}
+	_, isLeader := kv.rf.GetState()
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	op := Op{
+		ClientId:  args.ClientId,
+		CommandId: args.CommandId,
+		Key:       args.Key,
+		OpType:    "Get",
+	}
+
+	//调用raft的start函数对日志进行共识
+	index, _, _ := kv.rf.Start(op)
+
+	//根据index创建等待raft通道
+	ch := kv.getWaitCh(index)
+
+	//在函数返回时删除该等待通道
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.indexOpChan, index)
+		kv.mu.Unlock()
+	}()
+
+	// 设置超时ticker
+	timer := time.NewTicker(200 * time.Millisecond)
+	defer timer.Stop()
+
+	select {
+	case appliedOp := <-ch:
+		{
+			if appliedOp.ClientId == args.ClientId && appliedOp.CommandId == args.CommandId {
+				kv.mu.Lock()
+				_, exist := kv.kvMap[args.Key]
+				if !exist {
+					reply.Err = ErrNoKey
+				} else {
+					reply.Err = OK
+					reply.Value = kv.kvMap[args.Key]
+				}
+				kv.mu.Unlock()
+				return
+			} else {
+				reply.Err = ErrWrongLeader
+				return
+			}
+		}
+	case <-timer.C:
+		{
+			//println("1等待超时")
+			reply.Err = ErrWrongLeader
+			return
+		}
+	}
+
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	if kv.killed() {
+		reply.Err = ErrWrongLeader
+		return
+	}
+	_, isLeader := kv.rf.GetState()
+	if !isLeader {
+		reply.Err = ErrWrongLeader
+		return
+	}
+
+	op := Op{
+		CommandId: args.CommandId,
+		Key:       args.Key,
+		Value:     args.Value,
+		ClientId:  args.ClientId,
+		OpType:    args.Op,
+	}
+
+	//调用raft的start函数对日志进行共识
+	index, _, _ := kv.rf.Start(op)
+
+	//根据index创建等待raft通道
+	ch := kv.getWaitCh(index)
+
+	//在函数返回时删除该等待通道
+	defer func() {
+		kv.mu.Lock()
+		delete(kv.indexOpChan, index)
+		kv.mu.Unlock()
+	}()
+
+	// 设置超时ticker
+	timer := time.NewTicker(200 * time.Millisecond)
+	defer timer.Stop()
+
+	select {
+	case appliedOp := <-ch:
+		{
+			if appliedOp.ClientId == args.ClientId && appliedOp.CommandId == args.CommandId {
+				reply.Err = OK
+				return
+			} else {
+				reply.Err = ErrWrongLeader
+				return
+			}
+		}
+	case <-timer.C:
+		{
+			//println("2等待超时")
+			reply.Err = ErrWrongLeader
+			return
+		}
+	}
+}
+
+func (kv *KVServer) applyChanHandler() {
+	for {
+		select {
+		case applyMsg := <-kv.applyCh:
+			{
+				index := applyMsg.CommandIndex
+				op := applyMsg.Command.(Op)
+				//println("收到raft回复,", "op.OpType", op.OpType, "op.Key", op.Key)
+				if !kv.isDuplicate(op.ClientId, op.CommandId) {
+					//在KV中执行操作
+					kv.mu.Lock()
+					switch op.OpType {
+					case "Put":
+						kv.kvMap[op.Key] = op.Value
+					case "Append":
+						kv.kvMap[op.Key] += op.Value
+					}
+					kv.clientIdCommandId[op.ClientId] = op.CommandId
+					kv.mu.Unlock()
+				}
+				//通知操作已经执行
+				kv.getWaitCh(index) <- op
+			}
+		default:
+			time.Sleep(time.Millisecond * 5)
+		}
+
+	}
+}
+
+//判断某一客户端的某一条指令在本server中是否是重复的
+func (kv *KVServer) isDuplicate(clientId int64, commandId int) bool {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	lastCommandId, exist := kv.clientIdCommandId[clientId]
+	if !exist {
+		return false
+	}
+	return commandId <= lastCommandId
 }
 
 //
@@ -91,11 +267,14 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// You may need initialization code here.
+	kv.clientIdCommandId = make(map[int64]int)
+	kv.indexOpChan = make(map[int]chan Op)
+	kv.kvMap = make(map[string]string)
 
-	kv.applyCh = make(chan raft.ApplyMsg)
-	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
+	kv.applyCh = make(chan raft.ApplyMsg)                 //用于接收raft层传来的信息
+	kv.rf = raft.Make(servers, me, persister, kv.applyCh) //该server绑定的raft层
 
 	// You may need initialization code here.
-
+	go kv.applyChanHandler() //不断接受并处理raft传来的applyCh
 	return kv
 }
